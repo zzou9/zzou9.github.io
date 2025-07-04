5fdd6dbd690bf4b0862e50259fb0d5114d92c031
zzou9 <113187557+zzou9@users.noreply.github.com>
2025-07-04 20:41:59 +0000

    Deploying to gh-pages from @ zzou9/zzou9.github.io@40d352a24414de17330b406ebf48218eba529541 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,34 +4,34 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-publications",
-          title: "publications",
+          title: "Publications",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
           },
         },{id: "nav-repositories",
-          title: "repositories",
+          title: "Repositories",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
           },
         },{id: "nav-cv",
-          title: "cv",
+          title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
         },{id: "nav-activities",
-          title: "activities",
+          title: "Activities",
           description: "Teaching experiences, seminar talks, etc.",
           section: "Navigation",
           handler: () => {
